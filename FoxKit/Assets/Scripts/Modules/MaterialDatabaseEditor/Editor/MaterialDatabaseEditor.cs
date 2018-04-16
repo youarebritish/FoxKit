0ecb432b67b09f5dedbe92594fc5519a163d9785
Joey35233 <joey35233@gmail.com>
2018-04-16 18:24:19 -0400

    Added fmtt support to FoxKit

---
+++ FoxKit/Assets/Scripts/Modules/MaterialDatabaseEditor/Editor/MaterialDatabaseEditor.cs
@@ -0,0 +1,42 @@
+﻿namespace FoxKit.Modules.MaterialDatabaseEditor.Editor
+{
+    using System;
+
+    using UnityEngine;
+    using UnityEditor;
+
+    using FoxKit.Core;
+    using FoxKit.Modules.MaterialDatabaseEditor;
+    using FoxKit.Modules.MaterialDatabaseEditor.Exporter;
+
+    /// <summary>
+    /// Custom editor for MaterialDatabases.
+    /// </summary>
+    [CustomEditor(typeof(MaterialDatabase))]
+    public class MaterialDatabaseEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button("Export fmtt"))
+            {
+                var myTarget = (MaterialDatabase)this.target;
+
+                var exportPath = EditorUtility.SaveFilePanel(
+                    "Export fmtt",
+                    string.Empty,
+                    this.target.name + ".fmtt",
+                    "fmtt");
+
+                if (string.IsNullOrEmpty(exportPath))
+                {
+                    return;
+                }
+                MaterialDatabaseExporter.ExportMaterialDatabase(((MaterialDatabase)this.target).materialPresets as MaterialPreset[], exportPath);
+            }
+
+            this.DrawDefaultInspector();
+        }
+    }
+}
---
+++ FoxKit/Assets/Scripts/Modules/MaterialDatabaseEditor/MaterialDatabase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace FoxKit.Modules.MaterialDatabaseEditor
+{
+    /// <summary>
+    /// A material database which stores material presets.
+    /// </summary>
+    [CreateAssetMenu(fileName = "New Material Database", menuName = "FoxKit/Material Database")]
+    public class MaterialDatabase : ScriptableObject
+    {
+        public MaterialPreset[] materialPresets;
+    }
+}
