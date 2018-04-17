4b9b4753c14d968fc10328e5318bf4594225026b
youarebritish <bishop@projectbc.net>
2018-04-17 12:06:29 -0700

    Added .frld support.

---
+++ FoxKit/Assets/Scripts/Modules/RailBuilder/RailUniqueIdSet.cs
@@ -0,0 +1,13 @@
+﻿namespace FoxKit.Modules.RailBuilder
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Set of hashed rail IDs.
+    /// </summary>
+    [CreateAssetMenu(fileName = "New Rail Unique Id Set", menuName = "FoxKit/Rail/Rail Unique Id Set")]
+    public class RailUniqueIdSet : ScriptableObject
+    {
+        public uint[] Ids = new uint[0];
+    }
+}
