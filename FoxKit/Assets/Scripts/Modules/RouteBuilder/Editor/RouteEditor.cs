d0e54d5752c3add3385ecfe9caa19fba86604e82
youarebritish <bishop@projectbc.net>
2018-04-19 00:20:35 -0700

    Tidied up Route inspector code.

--- FoxKit/Assets/Scripts/Modules/RouteBuilder/Editor/RouteEditor.cs
+++ FoxKit/Assets/Scripts/Modules/RouteBuilder/Editor/RouteEditor.cs
@@ -14,7 +14,16 @@
         public override void OnInspectorGUI()
         {
             var route = this.target as Route;
-            
+
+            DrawToolShelf(route);
+            DrawSettings(route);
+            DrawNodeList(route);            
+
+            EditorUtility.SetDirty(target);
+        }
+
+        private static void DrawToolShelf(Route route)
+        {
             var iconAddNode = Resources.Load("UI/Route Builder/Buttons/routebuilder_button_new_node") as Texture;
             var iconParent = Resources.Load("UI/Route Builder/Buttons/routebuilder_button_parent") as Texture;
 
@@ -23,39 +32,40 @@
             GUILayout.FlexibleSpace();
 
             // Add node button
-            var addNodeContent = new GUIContent(iconAddNode, "Add a new node.");
-            if (GUILayout.Button(addNodeContent, GUILayout.MaxWidth(FoxKitUiUtils.BUTTON_DIMENSION), GUILayout.MaxHeight(FoxKitUiUtils.BUTTON_DIMENSION)))
+            if (FoxKitUiUtils.ToolButton(iconAddNode, "Add a new node."))
             {
                 route.AddNewNode();
             }
 
             // Select parent button
-            var selectParentContent = new GUIContent(iconParent, "Select parent RouteSet.");
-            if (GUILayout.Button(selectParentContent, GUILayout.MaxWidth(FoxKitUiUtils.BUTTON_DIMENSION), GUILayout.MaxHeight(FoxKitUiUtils.BUTTON_DIMENSION)))
+            if (FoxKitUiUtils.ToolButton(iconParent, "Select parent RouteSet."))
             {
                 UnitySceneUtils.Select(route.transform.parent.gameObject);
             }
+
             GUILayout.FlexibleSpace();
             EditorGUILayout.EndHorizontal();
+        }
 
-            //EditorGUILayout.Space();
-
+        private static void DrawSettings(Route route)
+        {
             Rotorz.Games.Collections.ReorderableListGUI.Title("Settings");
             route.Closed = EditorGUILayout.Toggle("Closed", route.Closed);
             route.TreatNameAsHash = EditorGUILayout.Toggle("Treat Name as Hash", route.TreatNameAsHash);
-
-            Rotorz.Games.Collections.ReorderableListGUI.Title("Nodes");
-            Rotorz.Games.Collections.ReorderableListGUI.ListField(route.Nodes, this.CustomListItem, this.DrawEmpty);
-
-            EditorUtility.SetDirty(target);
         }
 
-        private RouteNode CustomListItem(Rect position, RouteNode itemValue)
+        private static void DrawNodeList(Route route)
+        {
+            Rotorz.Games.Collections.ReorderableListGUI.Title("Nodes");
+            Rotorz.Games.Collections.ReorderableListGUI.ListField(route.Nodes, CustomListItem, DrawEmpty);
+        }
+
+        private static RouteNode CustomListItem(Rect position, RouteNode itemValue)
         {
             return EditorGUI.ObjectField(position, itemValue, typeof(RouteNode)) as RouteNode;
         }
 
-        private void DrawEmpty()
+        private static void DrawEmpty()
         {
             GUILayout.Label("Route has no nodes.", EditorStyles.miniLabel);
         }
--- FoxKit/Assets/Scripts/Utils/FoxKitUiUtils.cs
+++ FoxKit/Assets/Scripts/Utils/FoxKitUiUtils.cs
@@ -1,4 +1,6 @@
-﻿namespace FoxKit.Utils
+﻿using UnityEngine;
+
+namespace FoxKit.Utils
 {
     /// <summary>
     /// Helper functions for FoxKit UI.
@@ -9,5 +11,17 @@
         /// Height and width of FoxKit buttons.
         /// </summary>
         public const float BUTTON_DIMENSION = 32.0f;
+
+        /// <summary>
+        /// Draw a FoxKit tool button.
+        /// </summary>
+        /// <param name="icon">Icon to display on the button.</param>
+        /// <param name="tooltip">Tooltip text.</param>
+        /// <returns>True if the button was pressed.</returns>
+        public static bool ToolButton(Texture icon, string tooltip)
+        {
+            var content = new GUIContent(icon, tooltip);
+            return GUILayout.Button(content, GUILayout.MaxWidth(BUTTON_DIMENSION), GUILayout.MaxHeight(BUTTON_DIMENSION));
+        }
     }
 }
