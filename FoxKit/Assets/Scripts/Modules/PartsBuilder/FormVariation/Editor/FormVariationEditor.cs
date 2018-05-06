ce877d5580695ec51f8cbdb742a86924fbf1d2cc
Joey35233 <joey35233@gmail.com>
2018-05-06 18:49:04 -0400

    Technically-working editor code.

--- FoxKit/Assets/Scripts/Modules/PartsBuilder/FormVariation/Editor/FormVariationEditor.cs
+++ FoxKit/Assets/Scripts/Modules/PartsBuilder/FormVariation/Editor/FormVariationEditor.cs
@@ -6,14 +6,32 @@ namespace FoxKit.Modules.PartsBuilder.FormVariation.Editor
     using FoxKit.Modules.PartsBuilder.FormVariation;
     using FoxKit.Modules.PartsBuilder.FormVariation.Exporter;
 
+    using Rotorz.Games.Collections;
+
     /// <summary>
     /// Custom editor for FormVariations.
     /// </summary>
     [CustomEditor(typeof(FormVariation))]
     public class FormVariationEditor : Editor
     {
+        SerializedProperty _hiddenMeshGroupsProperty;
+        SerializedProperty _shownMeshGroupsProperty;
+        SerializedProperty _textureSwapProperty;
+        SerializedProperty _boneAttachmentProperty;
+        SerializedProperty _CNPAttachmentProperty;
+
+        private void OnEnable()
+        {
+            _hiddenMeshGroupsProperty = serializedObject.FindProperty("HiddenMeshGroups");
+            _shownMeshGroupsProperty = serializedObject.FindProperty("ShownMeshGroups");
+            _textureSwapProperty = serializedObject.FindProperty("TextureSwaps");
+            _boneAttachmentProperty = serializedObject.FindProperty("BoneAttachments");
+            _CNPAttachmentProperty = serializedObject.FindProperty("CNPAttachments");
+        }
+
         public override void OnInspectorGUI()
         {
+            //Export button
             EditorGUILayout.Space();
 
             if (GUILayout.Button("Export fmtt"))
@@ -33,7 +51,40 @@ namespace FoxKit.Modules.PartsBuilder.FormVariation.Editor
                 FormVariationExporter.ExportFormVariation(myTarget as FormVariation, exportPath);
             }
 
-            this.DrawDefaultInspector();
+            //List
+            drawFormVariation();
+        }
+
+        private void drawFormVariation()
+        {
+            serializedObject.Update();
+
+            ReorderableListGUI.Title("Hidden Mesh Groups");
+            ReorderableListGUI.ListField(_hiddenMeshGroupsProperty);
+
+            EditorGUILayout.Space();
+
+            ReorderableListGUI.Title("Shown Mesh Groups");
+            ReorderableListGUI.ListField(_shownMeshGroupsProperty);
+
+            EditorGUILayout.Space();
+
+            ReorderableListGUI.Title("Texture Swaps");
+            ReorderableListGUI.ListField(_textureSwapProperty);
+
+            EditorGUILayout.Space();
+
+            ReorderableListGUI.Title("Bone Attachments");
+            ReorderableListGUI.ListField(_boneAttachmentProperty);
+
+            EditorGUILayout.Space();
+
+            ReorderableListGUI.Title("Connection Point Attachments");
+            ReorderableListGUI.ListField(_CNPAttachmentProperty);
+
+            serializedObject.ApplyModifiedProperties();
+
+            base.DrawDefaultInspector();
         }
     }
 }
