ffcb1494cf836f363af51d3bd9f55714134b82fe
youarebritish <bishop@projectbc.net>
2018-06-19 11:01:37 -0700

    Very basic field classgen.

--- FoxKit/Assets/FoxKit/Modules/DataSet/Importer/ClassGenerator.cs
+++ FoxKit/Assets/FoxKit/Modules/DataSet/Importer/ClassGenerator.cs
@@ -1,5 +1,6 @@
 ﻿namespace FoxKit.Modules.DataSet.Importer
 {
+    using System;
     using System.IO;
     using System.Text;
 
@@ -8,6 +9,7 @@
     using UnityEditor;
 
     using UnityEngine;
+    using UnityEngine.Assertions;
 
     public static class ClassGenerator
     {
@@ -41,7 +43,8 @@
             // Open class block.
             stringBuilder.AppendLine("    {");
 
-            // TODO fields
+            AddStaticPropertyFields(stringBuilder, entity.StaticProperties, entity.ClassName);
+
             AddProperties(entity.ClassId, entity.Version, stringBuilder);
 
             // TODO MakeWritableStaticProperties
@@ -64,6 +67,87 @@
             stringBuilder.AppendLine(string.Empty);
             stringBuilder.AppendLine("        /// <inheritdoc />");
             stringBuilder.AppendLine(MakeVersionDeclaration(version));
+        }
+
+        private static void AddStaticPropertyFields(StringBuilder stringBuilder, Core.PropertyInfo[] properties, string className)
+        {
+            foreach (var property in properties)
+            {
+                AddStaticPropertyField(stringBuilder, property, className);
+                stringBuilder.AppendLine(string.Empty);
+            }
+        }
+
+        private static void AddStaticPropertyField(StringBuilder stringBuilder, Core.PropertyInfo property, string className)
+        {
+            // TODO Initialize string to string.Empty
+            // TODO Handle entity ptrs
+            // TODO Containers
+            stringBuilder.AppendLine($"        [SerializeField, Modules.DataSet.Property(\"{className}\")]");
+            stringBuilder.AppendLine($"        private {GetTypeString(property.Type)} {property.Name};");
+        }
+
+        private static string GetTypeString(Core.PropertyInfoType type)
+        {
+            switch (type)
+            {
+                case Core.PropertyInfoType.Int8:
+                    return "sbyte";
+                case Core.PropertyInfoType.UInt8:
+                    return "byte";
+                case Core.PropertyInfoType.Int16:
+                    return "short";
+                case Core.PropertyInfoType.UInt16:
+                    return "ushort";
+                case Core.PropertyInfoType.Int32:
+                    return "int";
+                case Core.PropertyInfoType.UInt32:
+                    return "uint";
+                case Core.PropertyInfoType.Int64:
+                    return "long";
+                case Core.PropertyInfoType.UInt64:
+                    return "ulong";
+                case Core.PropertyInfoType.Float:
+                    return "float";
+                case Core.PropertyInfoType.Double:
+                    return "double";
+                case Core.PropertyInfoType.Bool:
+                    return "bool";
+                case Core.PropertyInfoType.String:
+                    return "string";
+                case Core.PropertyInfoType.Path:
+                    return "string";
+                case Core.PropertyInfoType.EntityPtr:
+                    return "object";    // TODO
+                case Core.PropertyInfoType.Vector3:
+                    return "Vector3";
+                case Core.PropertyInfoType.Vector4:
+                    return "Vector4";
+                case Core.PropertyInfoType.Quat:
+                    return "Quaternion";
+                case Core.PropertyInfoType.Matrix3:
+                    return "Matrix3x3";
+                case Core.PropertyInfoType.Matrix4:
+                    return "Matrix4x4";
+                case Core.PropertyInfoType.Color:
+                    return "Color";
+                case Core.PropertyInfoType.FilePtr:
+                    return "UnityEngine.Object";
+                case Core.PropertyInfoType.EntityHandle:
+                    return "object";    // TODO
+                case Core.PropertyInfoType.EntityLink:
+                    return "EntityLink";
+                case Core.PropertyInfoType.PropertyInfo:
+                    Assert.IsTrue(false, "Unsupported property type: PropertyInfo.");
+                    break;
+                case Core.PropertyInfoType.WideVector3:
+                    Assert.IsTrue(false, "Unsupported property type: WideVector3.");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(type), type, null);
+            }
+
+            return "ERROR";
         }
 
         private static void AddUsingStatements(StringBuilder stringBuilder)
