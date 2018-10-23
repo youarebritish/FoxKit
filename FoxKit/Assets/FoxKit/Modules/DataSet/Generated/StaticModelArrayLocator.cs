837b1aba5f49a363016eea628cae4e3a172b5acb
youarebritish <bishop@projectbc.net>
2018-10-23 16:58:57 -0700

    Updated FoxLib version

--- FoxKit/Assets/FoxKit/Modules/DataSet/Generated/StaticModelArrayLocator.cs
+++ FoxKit/Assets/FoxKit/Modules/DataSet/Generated/StaticModelArrayLocator.cs
@@ -25,22 +25,5 @@ namespace FoxKit.Modules.DataSet
 
         /// <inheritdoc />
         public override ushort Version => 1;
-
-        /// <inheritdoc />
-        public override List<Core.PropertyInfo> MakeWritableStaticProperties(Func<Entity, ulong> getEntityAddress, Func<EntityLink, Core.EntityLink> convertEntityLink)
-        {
-            var parentProperties = base.MakeWritableStaticProperties(getEntityAddress, convertEntityLink);
-            return parentProperties;
-        }
-
-        /// <inheritdoc />
-        protected override void ReadProperty(Core.PropertyInfo propertyData, Importer.EntityFactory.EntityInitializeFunctions initFunctions)
-        {
-            base.ReadProperty(propertyData, initFunctions);
-
-            switch (propertyData.Name)
-            {
-            }
-        }
     }
 }
