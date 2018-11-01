99370e069db0d04ade1a6b2c6a41475d56ce4af6
youarebritish <bishop@projectbc.net>
2018-10-31 21:26:00 -0700

    Added proof of concept StringMap inspector.

---
+++ FoxKit/Assets/FoxKit/Utils/Editor/StringMapAddKeyWindow.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEditor;
+
+using UnityEngine;
+
+public class StringMapAddKeyWindow : EditorWindow
+{
+    private string newKey = string.Empty;
+    private List<string> invalidKeys;
+    private Action<string> onConfirm;
+
+    public static void Create(List<string> invalidKeys, Action<string> onConfirm)
+    {
+        var window = ScriptableObject.CreateInstance(typeof(StringMapAddKeyWindow)) as StringMapAddKeyWindow;
+        window.titleContent = new GUIContent("Add StringMap Key");
+        window.minSize = new Vector2(250, 90);
+        window.maxSize = new Vector2(400, 90);
+        window.invalidKeys = invalidKeys;
+        window.onConfirm = onConfirm;
+        window.ShowAuxWindow();
+    }
+
+    void OnGUI()
+    {
+        this.newKey = EditorGUILayout.TextField(this.newKey);
+
+        if (string.IsNullOrEmpty(this.newKey))
+        {
+            GUI.enabled = false;
+        }
+        else if (this.invalidKeys.Contains(this.newKey))
+        {
+            EditorGUILayout.HelpBox("The given key is already present in the StringMap.", MessageType.Error);
+            GUI.enabled = false;
+        }
+        else
+        {
+            GUI.enabled = true;
+        }
+
+        if (!GUILayout.Button("Insert", GUILayout.ExpandWidth(false)))
+        {
+            return;
+        }
+
+        this.onConfirm(this.newKey);
+        this.Close();
+    }
+}
---
+++ FoxKit/Assets/FoxKit/Utils/Editor/StringMapGUI.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Rotorz.Games.Collections;
+
+using UnityEditor;
+
+using UnityEngine;
+
+public static class StringMapGUI
+{
+    public static void StringMapField<TValue>(IDictionary<string, TValue> dictionary, ReorderableListControl.ItemDrawer<TValue> drawItem)
+    {
+        // Draw entries.
+        string keyToRemove = null;
+
+        var changedEntries = new List<KeyValuePair<string, TValue>>();
+        foreach (var kvp in dictionary)
+        {
+            EditorGUILayout.BeginHorizontal();
+
+            var wasGUIenabled = GUI.enabled;
+            GUI.enabled = false;
+            EditorGUILayout.TextField(kvp.Key);
+            GUI.enabled = wasGUIenabled;
+
+            var position = EditorGUILayout.GetControlRect(false, 16);
+
+            var currentValue = kvp.Value;
+            var newValue = drawItem(position, currentValue);
+
+            // Draw remove button.
+            if (GUILayout.Button("-", EditorStyles.miniButton, GUILayout.ExpandWidth(false)))
+            {
+                keyToRemove = kvp.Key;
+            }
+            else
+            {
+                changedEntries.Add(new KeyValuePair<string, TValue>(kvp.Key, newValue));
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        // Remove whichever key the user selected, if any.
+        if (keyToRemove != null)
+        {
+            dictionary.Remove(keyToRemove);
+        }
+
+        // Update any changed entries.
+        foreach (var kvp in changedEntries)
+        {
+            dictionary[kvp.Key] = kvp.Value;
+        }
+
+        // Draw add entry section.
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        if (GUILayout.Button("+", EditorStyles.miniButton, GUILayout.ExpandWidth(false)))
+        {
+            StringMapAddKeyWindow.Create(
+                dictionary.Keys.ToList(),
+                newKey => dictionary.Add(newKey, default(TValue)));
+        }
+
+        EditorGUILayout.EndHorizontal();
+    }
+}
