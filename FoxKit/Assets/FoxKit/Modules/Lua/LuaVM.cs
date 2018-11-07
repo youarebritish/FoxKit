0590acf87367d4c7f390ce22b3da54a5a8ed3017
youarebritish <bishop@projectbc.net>
2018-11-06 22:58:29 -0800

    Started on LuaVM.

---
+++ FoxKit/Assets/FoxKit/Modules/Lua/LuaVM.cs
@@ -0,0 +1,77 @@
+﻿namespace FoxKit.Modules.Lua
+{
+    using KopiLua;
+
+    /// <summary>
+    /// Global Lua state.
+    /// </summary>
+    public class LuaVM
+    {
+        /// <summary>
+        /// The print() library.
+        /// </summary>
+        private static readonly Lua.luaL_Reg[] PrintLib = { new Lua.luaL_Reg("print", Print), new Lua.luaL_Reg(null, null), };
+
+        /// <summary>
+        /// The global instance.
+        /// </summary>
+        private static LuaVM instance;
+
+        /// <summary>
+        /// Gets the Lua state.
+        /// </summary>
+        public Lua.lua_State L { get; private set; }
+
+        /// <summary>
+        /// Create the global LuaVM instance.
+        /// </summary>
+        /// <returns>The global LuaVM instance.</returns>
+        public static LuaVM Create()
+        {
+            var lua = Lua.lua_open();
+            instance = new LuaVM { L = lua };
+
+            // Initialize Lua state.
+            Lua.luaL_openlibs(lua);
+
+            // Overwrite default print() function to write to Debug.Log().
+            Lua.lua_getglobal(lua, "_G");
+             Lua.luaL_register(lua, null, PrintLib);
+
+            // TODO Initialize libraries
+
+            // TODO expose native types
+
+            return instance;
+        }
+
+        /// <summary>
+        /// Get the global LuaVM instance.
+        /// </summary>
+        /// <returns>The global LuaVM instance.</returns>
+        public static LuaVM GetInstance()
+        {
+            return instance;
+        }
+
+        /// <summary>
+        /// Lua print function.
+        /// </summary>
+        /// <param name="lua">
+        /// The Lua state.
+        /// </param>
+        /// <returns>
+        /// The Lua result code.
+        /// </returns>
+        private static int Print(Lua.lua_State lua)
+        {
+            var nargs = Lua.lua_gettop(lua);
+            for (var i = 1; i <= nargs; i++)
+            {
+                UnityEngine.Debug.Log(Lua.lua_tostring(lua, i));
+            }
+
+            return 0;
+        }
+    }
+}
